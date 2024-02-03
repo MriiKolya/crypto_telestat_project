d386f51904794dfd7b938e412905d9cdcb2d8f49
MriiKolya <kolyamriy16@gmail.com>
2024-02-04 00:45:27 +0200

    fix_adapters

--- lib/core/di/hive_adapters.dart
+++ lib/core/di/hive_adapters.dart
@@ -1,6 +1,6 @@
-import 'package:crypto_telestat/features/trending_coins_list/model/coin_data_model.dart';
-import 'package:crypto_telestat/features/trending_coins_list/model/coin_model.dart';
-import 'package:crypto_telestat/features/trending_coins_list/model/price_change_percent.dart';
+import 'package:crypto_telestat/features/trending_coins_list/data/model/coin_data_model.dart';
+import 'package:crypto_telestat/features/trending_coins_list/data/model/coin_model.dart';
+import 'package:crypto_telestat/features/trending_coins_list/data/model/price_change_percent.dart';
 import 'package:crypto_telestat/features/trending_nfts_list/model/nfts_data_model.dart';
 import 'package:crypto_telestat/features/trending_nfts_list/model/nfts_model.dart';
 import 'package:hive/hive.dart';
